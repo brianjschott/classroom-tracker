543b195501c964c5c5e1818856a49effaffc55e0
Brian Schott <bschott@cgps.org>
2021-03-10 08:55:08 -0500

    Initial Commit

---
+++ classroom-tracker-v3/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  classroom-tracker-v3
+//
+//  Created by Brian Schott on 3/10/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ classroom-tracker-v3Tests/classroom_tracker_v3Tests.swift
@@ -0,0 +1,33 @@
+//
+//  classroom_tracker_v3Tests.swift
+//  classroom-tracker-v3Tests
+//
+//  Created by Brian Schott on 3/10/21.
+//
+
+import XCTest
+@testable import classroom_tracker_v3
+
+class classroom_tracker_v3Tests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
